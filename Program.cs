c1ff54f97af61977179e6fd6a9cceeb8dc2b2468
Chris Gray <Chris.Gray@guestline.com>
2018-09-21 14:09:47 +0100

    Starting from a blank program.cs

--- Program.cs
+++ Program.cs
@@ -6,28 +6,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            int fizzBuzz;
-
-            for (fizzBuzz = 1; fizzBuzz <= 100; fizzBuzz++)
-            {
-                if (fizzBuzz % 3 == 0 && fizzBuzz % 5 == 0)
-                {
-                    Console.WriteLine("fizzbuzz");
-                }
-                else if (fizzBuzz % 5 == 0)
-                {
-                    Console.WriteLine("buzz");
-                }
-                else if (fizzBuzz % 3 == 0)
-                {
-                    Console.WriteLine("fizz");
-                }
-                else
-                {
-                    Console.WriteLine(fizzBuzz);
-                }
-            }
-            Console.ReadKey();
+            
         }
     }
 }
