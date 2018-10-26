d5c15604be0546792ae8d19103a1f2cc66f6adfd
Chris Gray <Chris.Gray@guestline.com>
2018-10-26 15:17:18 +0100

    Condensed all the code into one method/

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,7 +4,6 @@ namespace fizzbuzz
 {
     public class FizzBuzz
     {
-
         public int FizzBuzzNumber(int fizzBuzzNumber)
         {
             for (fizzBuzzNumber = 1; fizzBuzzNumber <= 100; fizzBuzzNumber++)
@@ -30,45 +29,7 @@ namespace fizzbuzz
             Console.ReadKey();
             return fizzBuzzNumber;
         }
-        /*
-        public int Fizz(int fizzNumber)
-        {
-            
-            for (fizzNumber = 1; fizzNumber <= 100; fizzNumber++)
-            {
-                if (fizzNumber % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else
-                {
-                    Console.WriteLine(fizzNumber);
-                }
-                
-            }
-            Console.ReadKey();
-            return fizzNumber;
-        }
-
-        public int Buzz(int buzzNumber)
-        {
-            
-            for (buzzNumber = 1; buzzNumber <= 100; buzzNumber++)
-            {
-                if (buzzNumber % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(buzzNumber);
-                }
-
-            }
-            Console.ReadKey();
-            return buzzNumber;
-        }
-        */
+        
         public static void Main(string[] args)
         {
             FizzBuzz fb = new FizzBuzz();
